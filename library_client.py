8e81c1206d4440c11178b26f683a46df0a6dca2d
Jason Nguyen <khaivnguyen02@gmail.com>
2023-07-27 19:57:56 -0700

    boilerplate

--- library_client.py
+++ library_client.py
@@ -1,3 +1,7 @@
+import sqlite3
+conn = sqlite3.connect('library.db')
+cursor = conn.cursor()
+
 def printMenu():
     print("==================== Library Client ====================")
     print("1.Find an item")
@@ -9,16 +13,62 @@ def printMenu():
     print("7.Volunteer for the library")
     print("8.Ask for help from a librarian")
     print("9.Exit")
+    print("========================================================")
+
+def find_item():
+    pass
+
+def borrow_item(): 
+    pass
+
+def return_item():
+    pass
+
+def donate_item():
+    pass
+
+def find_event():
+    pass
+
+def register_event():
+    pass
+
+def volunteer():
+    pass
+
+def get_contact_information():
+    query = "SELECT name, email FROM Personnel WHERE position = 'Librarian' "
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
 def main():
     isRunning = True
     while (isRunning):
         printMenu()
-        userChoice = input("Please input the number of your option: ")
-
+        userChoice = int(input("Please input the number of your option: "))
         print("========================================================")
-        print("Exited")
-        return
+
+        if (userChoice == 1):
+            find_item()
+        elif (userChoice == 2):
+            borrow_item()
+        elif (userChoice == 3):
+            return_item()
+        elif (userChoice == 4):
+            donate_item()
+        elif (userChoice == 5):
+            find_event()
+        elif (userChoice == 6):
+            register_event()
+        elif (userChoice == 7):
+            volunteer()
+        elif (userChoice == 8):
+            get_contact_information()
+        else:
+            print("Exited")
+            return
 
 if __name__ == "__main__":
     main()
