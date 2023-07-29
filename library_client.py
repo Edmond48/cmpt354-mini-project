73508d3658fc560cb73d397a235eb01dadb8117a
Edmond <duong.nghuu2003@gmail.com>
2023-07-29 10:51:30 -0700

    Implement finding items; modify UI

--- library_client.py
+++ library_client.py
@@ -1,9 +1,10 @@
 import sqlite3
+
+# global variable
 conn = sqlite3.connect('library.db')
-cursor = conn.cursor()
 
 def printMenu():
-    print("\n\n==================== Library Client ====================")
+    print("\n==================== Library Client ====================")
     print("1.Find an item")
     print("2.Borrow an item")
     print("3.Return an item")
@@ -13,10 +14,35 @@ def printMenu():
     print("7.Volunteer for the library")
     print("8.Ask for help from a librarian")
     print("9.Exit")
-    print("========================================================")
 
 def find_item():
-    pass
+    print("\n==================== Finding Items ====================")
+    print("Please enter the following details of the item (can be left blank):")
+    title = input("Title: ")
+    author = input("Author:")
+    year = input("Year: ")
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM Entries "
+    if(title or author or year):
+        query += "WHERE "
+        query += "title=:queryTitle " if title else "TRUE "
+        query += "AND "
+        query += "author=:queryAuthor " if author else "TRUE "
+        query += "AND "
+        query += "year=:queryYear" if year else "TRUE "
+
+    cursor.execute(query, {"queryTitle":title, "queryAuthor":author, "queryYear":year})
+    rows = cursor.fetchall()
+
+    print("\n==================== Results ====================")
+    if rows:
+        for row in rows:
+            print(row)
+    else:
+        print("\nNo item(s) found")
+    
+    input("\nPlease press Enter to continue")
 
 def borrow_item(): 
     pass
@@ -37,40 +63,50 @@ def volunteer():
     pass
 
 def get_contact_information():
+    cursor = conn.cursor()
+
     query = "SELECT name, email FROM Personnel WHERE position = 'Librarian' "
     cursor.execute(query)
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+
+    if rows:
+        print("\nPlease contact our librarians from the following list:")
+        for row in rows:
+            print(" - " + row[0] + ": " + row[1])
+    else:
+        print("\nSorry. No librarian is available at the moment")
+        
+    input("\nPlease press Enter to continue")
 
 
 
 def main():
     isRunning = True
-    while (isRunning):
-        printMenu()
-        userChoice = int(input("Please input the number of your option: "))
-        print("========================================================")
+    with conn:
+        while (isRunning):
+            printMenu()
+            userChoice = int(input("Please input the number of your option: "))
 
-        if (userChoice == 1):
-            find_item()
-        elif (userChoice == 2):
-            borrow_item()
-        elif (userChoice == 3):
-            return_item()
-        elif (userChoice == 4):
-            donate_item()
-        elif (userChoice == 5):
-            find_event()
-        elif (userChoice == 6):
-            register_event()
-        elif (userChoice == 7):
-            volunteer()
-        elif (userChoice == 8):
-            get_contact_information()
-        else:
-            print("Exited")
-            return
+            if (userChoice == 1):
+                find_item()
+            elif (userChoice == 2):
+                borrow_item()
+            elif (userChoice == 3):
+                return_item()
+            elif (userChoice == 4):
+                donate_item()
+            elif (userChoice == 5):
+                find_event()
+            elif (userChoice == 6):
+                register_event()
+            elif (userChoice == 7):
+                volunteer()
+            elif (userChoice == 8):
+                get_contact_information()
+            else:
+                isRunning = False
+        print("========================================================")
+        print("Exited")
 
 if __name__ == "__main__":
     main()
